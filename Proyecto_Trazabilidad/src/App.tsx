303d6e202fe7681efc1571a5ae255d341444ae1c
yoryi404 <jalegrem04@iesalbarregas.es>
2026-03-17 15:27:13 +0100

    Crear Footer y añadir lo necesario al aplicativo para que funcione

--- Proyecto_Trazabilidad/src/App.tsx
+++ Proyecto_Trazabilidad/src/App.tsx
@@ -1,13 +1,18 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
 import './styles/index.css'
 
 function App() {
-
   return (
-    <div>
-      <Header></Header>
-      <Hero></Hero>
+    <div className="min-h-screen bg-gray-50 flex flex-col font-sans selection:bg-blue-200 selection:text-blue-900">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
     </div>
   )
 }
